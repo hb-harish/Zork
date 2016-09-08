a65da05f1d777c2cd630dedcf83871e2b9e170da
harish <hb.harishbalaji@gmail.com>
2016-09-08 09:42:48 -0400

    zork extended like crazy

--- src/Zork.java
+++ src/Zork.java
@@ -5,6 +5,7 @@ import java.util.Random;
 public class Zork 
 {
 	static int amt =0;
+	static int curr =0;
 	static int flag,mflag;
 	static int tflag =0;
 	static int r1 = 0;
@@ -15,15 +16,16 @@ public class Zork
 	static int r6 = 0;
 	static int r7 = 0;
 	static int r8 = 0;
-	Random theif = new Random();
-	static int th =  1 + theif.nextInt(8);
+	static Random theif = new Random();
+	static int th =  2 + theif.nextInt(8);
+	static Scanner sc = new Scanner(System.in);
 	public static void money()
 	{
 		Random mon = new Random();
 		int m =  mon.nextInt(1001);
-		System.out.println("You found " + m + "$\n");
+		System.out.println("You found $" + m + "\n");
 		amt = amt + m;
-		System.out.println("your total money is " + amt + "$\n");
+		System.out.println("your total money is $" + amt + "\n");
 	}
 	public static void theif()
 	{
@@ -33,26 +35,24 @@ public class Zork
 		System.out.println("He wants to gamble you for your money\n");
 		System.out.println("You agree because he has a gun\n");
 		System.out.println("Call heads\"1\" or tails \"2\"");
-		int ch = sc.nextInt();
+		int o = sc.nextInt();
 		if (coin==0)
 			System.out.println("Theif flips Heads\n");
 		else if (o==0)
 			System.out.println("Theif flips Tails\n");
-		if (coin==ch)
+		if (coin==o)
 			System.out.println("Theif is sad and leaves you alone\n");
 		else 
 			System.out.println("You lose your money\n");
 			amt = 0;
-		
-		amt = amt + m;
-		System.out.println("your total money is " + amt + "$\n");
+		System.out.println("your total money is $" + amt + "\n");
 	}
 	public static int room1(int p)
 	{
 		r1++;
-		Scanner sc = new Scanner(System.in);
+		curr =1;
 		System.out.println("\nYou are in a foyer in an old house.\nIt has a dead rhino\n");
-		System.out.println("You can go north \"1\" or Exit the house \"0\"\n");
+		System.out.println("You can go north \"1\" or walk out of the house \"0\"\n");
 		int o = sc.nextInt(); 
 		if (o==1)
 			p = 2;
@@ -66,9 +66,9 @@ public class Zork
 	public static int room2(int p)
 	{
 		r2++;
-		Scanner sc = new Scanner(System.in);
+		curr =2;
 		System.out.println("\nYou reach the front room and see a beautiful piano\n");
-		System.out.println("You can go south \"1\", west \"2\" east \"3\" or quit \"0\"\n");
+		System.out.println("You can go south \"1\", west \"2\" east \"3\" or quit without your money or gold \"0\"\n");
 		int o = sc.nextInt(); 
 		if (o==1)
 			p = 1;
@@ -86,9 +86,9 @@ public class Zork
 	public static int room3(int p)
 	{
 		r3++;
-		Scanner sc = new Scanner(System.in);
+		curr =3;
 		System.out.println("\nYou are in the library of doom. There are spiders eveywhere\n");
-		System.out.println("You can go north \"1\", east \"2\" or quit \"0\"\n");
+		System.out.println("You can go north \"1\", east \"2\" or quit without your money or gold \"0\"\n");
 		int o = sc.nextInt(); 
 		if (o==1)
 			p = 5;
@@ -104,9 +104,9 @@ public class Zork
 	public static int room4(int p)
 	{
 		r4++;
-		Scanner sc = new Scanner(System.in);
+		curr =4;
 		System.out.println("\nYou are in Hell's Kitchen. There are bats everywhere\n");
-		System.out.println("You can go west \"1\", north \"2\" or quit \"0\"\n");
+		System.out.println("You can go west \"1\", north \"2\" or quit without your money or gold \"0\"\n");
 		int o = sc.nextInt(); 
 		if (o==1)
 			p = 2;
@@ -120,11 +120,11 @@ public class Zork
 	
 	}
 	public static int room5(int p)
-	{
+	{		
 		r5++;
-		Scanner sc = new Scanner(System.in);
+		curr =5;
 		System.out.println("\nYou have reached a dusty dinning room.\n On the table is an empty box\n");
-		System.out.println("You can go south \"1\" or quit \"0\"\n");
+		System.out.println("You can go south \"1\" or quit without your money or gold \"0\"\n");
 		int o = sc.nextInt(); 
 		if (o==1)
 			p = 3;
@@ -138,12 +138,12 @@ public class Zork
 	public static int room6(int p)
 	{
 		r6++;
-		Scanner sc = new Scanner(System.in);
+		curr =6;
 		System.out.println("\nYou have reached the vault.\nThere are 3 scary walking skeletons\n");
 		if (flag==0)
-			System.out.println("You can go east \"1\" or quit \"0\"\n");
+			System.out.println("You can go east \"1\" or quit without your money or gold \"0\"\n");
 		else if (flag == 1)
-			System.out.println("You can go east (parlor) \"8\" or to secret room \"9\" or quit \"0\"\n");
+			System.out.println("You can go east (parlor) \"8\" or to secret room \"9\" or quit without your money or gold \"0\"\n");
 		int o = sc.nextInt();
 		Random rnd = new Random();
 		int r = 1+ rnd.nextInt(4);
@@ -173,9 +173,9 @@ public class Zork
 	public static int room7(int p)
 	{
 		r7++;
-		Scanner sc = new Scanner(System.in);
+		curr =7;
 		System.out.println("\nYou have the reached the parlor.\nYou found a treasure chest.\n");
-		System.out.println("You can go south \"1\", west \"2\" or quit \"0\"\n");
+		System.out.println("You can go south \"1\", west \"2\" or quit without your money or gold \"0\"\n");
 		int o = sc.nextInt(); 
 		if (o==1)
 			p = 4;
@@ -191,11 +191,11 @@ public class Zork
 	public static int room8(int p)
 	{
 		r8++;
-		Scanner sc = new Scanner(System.in);
+		curr =8;
 		System.out.println("\nYou have the reached the secret room and found a mountain of gold.\n");
-		System.out.println("You can go west \"1\" or quit \"0\"\n");
+		System.out.println("You can go west \"1\" or quit without your money or gold \"0\"\n");
 		int o = sc.nextInt(); 
-		if (o==1)			if (p==1 && r1==0) money();
+		if (o==1)		
 			p = 6;
 		else if (o==0)
 			p =0;
@@ -253,6 +253,27 @@ public class Zork
 			}
 		}while(p!=0);
 		System.out.println("\nThe number of rooms visited is " + c + "\n");
+		System.out.println("\nThe number of times you have seen the dead rhino is " + r1 + "\n");
+		System.out.println("\nThe number of times you have seen the beautiful piano is " + r2 + "\n");
+		System.out.println("\nThe number of times you have seen the dcreepy spiders " + r3 + "\n");
+		System.out.println("\nThe number of times you have seen the dscary bats is " + r4 + "\n");
+		System.out.println("\nThe number of times you have seen the empty box is " + r5 + "\n");
+		System.out.println("\nThe number of times you have seen the walking skeletons is " + r6 + "\n");
+		System.out.println("\nThe number of times you have seen the treasure chest " + r7 + "\n");
+		if (r8>=1 && curr==1)
+		{
+			System.out.println("\nCongratualtions you walked out with the mountain of gold\n");
+			System.out.println("\nAlso, the total amount of money you found is " + amt + "\n");
+		}
+		else if (curr==1)
+		{
+			System.out.println("\nThe total amount of money you found is $" + amt + "\n");
+			System.out.println("\nHowever you did not find the hidden gold\n");
+		}
+		else
+		{
+			System.out.println("\nYou do not get any money or gold for being a QUITTER\n");
+		}
 		Random rnd2 = new Random();
 		int g = 1+ rnd2.nextInt(4);
 		if (g == 4)
